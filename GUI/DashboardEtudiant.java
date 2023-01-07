3915a8af1923a5578b7a165d5c318ebf57be5f7e
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2023-01-08 00:25:46 +0100

    display Etudiant data on dashboard in progress ...

--- GUI/DashboardEtudiant.java
+++ GUI/DashboardEtudiant.java
@@ -1,6 +1,7 @@
 package GUI;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JTable;
 
 import GestionTables.GestionEtudiant;
@@ -9,32 +10,51 @@ import Tables.Etudiant;
 public class DashboardEtudiant extends JFrame {
 
     JFrame frame;
-    JTable jt;
+    JLabel libelleId;
+    JLabel libelleNom;
+    JLabel libellePrenom;
+    JLabel libelleLogin;
+    JLabel libelleIdClasse;
     Etudiant etudiant;
     GestionEtudiant gestionEtudiant;
 
-    public DashboardEtudiant() {
+    public DashboardEtudiant(String username, String password) {
 
         frame = new JFrame();
         etudiant = new Etudiant();
         gestionEtudiant = new GestionEtudiant();
+        etudiant = gestionEtudiant.authentifier(password, username);
+        System.out.println("Dashboard GUI: "+ etudiant);
 
-        /* table */
-        String titre[] = { "ID", "NAME", "SALARY" };
-        Object data[][] = { 
-                { "101", "Amit", "670000" },
-                { "102", "Jai", "780000" },
-                { "101", "Sachin", "700000" } 
-        };
-        
-        jt = new JTable(data, titre);
-        jt.setBounds(30, 40, 200, 300);
-        getContentPane().add(jt);
-        /* table */
+        int id = etudiant.getIdEtudiant();
+        String nom = etudiant.getNom();
+        String prenom = etudiant.getPrenom();
+        String login = etudiant.getLogin();
+        int id_class = etudiant.getIdClasse();
+      
+        String id_etudiant = Integer.toString(id);
+        String id_classe = Integer.toString(id_class);
 
+        libelleId = new JLabel(id_etudiant);
+        libelleNom = new JLabel(nom);
+        libellePrenom = new JLabel(prenom);
+        libelleLogin = new JLabel(login);
+        libelleIdClasse = new JLabel(id_classe);
+
+        libelleId.setBounds(10,30,200,35);
+        libelleNom.setBounds(110, 30, 200, 35);
+        libellePrenom.setBounds(210, 30, 200, 35);
+        libelleLogin.setBounds(310, 30, 200, 35);
+        libelleIdClasse.setBounds(410, 30, 200, 35);
+
+
+        add(libelleId);
+        add(libelleNom);
+        add(libellePrenom);
+        add(libelleLogin);
+        add(libelleIdClasse);
         setTitle("Gestion Absence ~ Etudiant Dashboard");
-        add(jt);
-        setSize(600, 500);
+        setSize(1000, 700);
         setLayout(null);
         setVisible(true);
     }
