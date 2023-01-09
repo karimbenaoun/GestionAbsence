275ebd35ef4a7b6beffa84a7ff1d648848cff858
YASSCODER <72893609+YASSCODER@users.noreply.github.com>
2023-01-09 19:53:20 +0100

    trying to fix some bugs

--- GUI/DashboardEtudiant.java
+++ GUI/DashboardEtudiant.java
@@ -12,6 +12,9 @@ import java.util.Date;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import com.mysql.jdbc.PreparedStatement;
 
 import GestionTables.GestionEtudiant;
 import Tables.Etudiant;
@@ -38,6 +41,8 @@ public class DashboardEtudiant extends JFrame {
         db = new Base();
         etudiant = new Etudiant();
         gestionEtudiant = new GestionEtudiant();
+        tableau1 = new JTable();
+        DefaultTableModel dtm = new DefaultTableModel(0, 0);
         etudiant = gestionEtudiant.authentifier(password, username);
         System.out.println("Dashboard GUI: "+ etudiant);
 
@@ -83,8 +88,15 @@ public class DashboardEtudiant extends JFrame {
         try {
             Statement stm = this.cnn.prepareStatement(query);
             ResultSet res = stm.executeQuery(query);
-            int i = 0;
-            if (res.next()) {
+
+            query = "SELECT COUNT(*) as jk FROM absence WHERE id_etudiant = '" + id + "'";
+            Statement stm2 = this.cnn.prepareStatement(query);
+            ResultSet res2 = stm2.executeQuery(query);
+            int numRow = res2.getRow();
+            res2.next();
+            System.out.println("Rows /> " + res2.getInt("jk"));
+            while (res.next()) {
+
                 int numSea = res.getInt(1);
                 String numSeance = Integer.toString(numSea);
 
@@ -100,15 +112,17 @@ public class DashboardEtudiant extends JFrame {
                 Date date = res.getDate(2);
                 DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
                 String strDate = dateFormat.format(date);
+
+                System.out.println("num seance : "+numSeance + " date : "+ strDate+ " id etudiant : " + idEtt + " id enseignant : "+idEs+ " id matiere : " + idMt  );
+
                 String column[] = {"Seance", "Date", "Etudiant", "Enseignant", "Matiere"};
-                Object data[][] = {
-                        { numSeance, strDate, idEtt, idEs, idMt },
-                        { numSeance, strDate, idEtt, idEs, idMt },
-                        { numSeance, strDate, idEtt, idEs, idMt }
-                };
-                tableau1 = new JTable(data,column);
-                tableau1.setBounds(10, 70, 600, 500);
-                add(tableau1);
+                dtm.setColumnIdentifiers(column);
+                for(int i = 0; i < res2.getInt("jk");i++){
+                    
+
+                        dtm.addRow(new Object[] { numSea, date, idEt, idEs, idMat,});
+                        tableau1.setModel(dtm);
+                }
             }
             
         } catch (SQLException e) {
@@ -150,7 +164,6 @@ public class DashboardEtudiant extends JFrame {
         add(libelleEnseignant);
         add(libelleMatier);
         add(tableau1);
-        
         add(libelleNom);
         add(libellePrenom);
         add(libelleIdClasse);
